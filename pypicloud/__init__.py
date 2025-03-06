fecf3a18055f3e71081fa1bd14103cdf07dd4844
Haejoon Kim <onsealeatang@gmail.com>
2025-03-06 18:01:25 +0900

    Bump version: 1.3.12 → 1.4.0

--- pypicloud/__init__.py
+++ pypicloud/__init__.py
@@ -13,7 +13,7 @@ from .locator import FormattedScrapingLocator, SimpleJsonLocator
 from .route import Root
 from .util import EnvironSettings
 
-__version__ = "1.3.12"
+__version__ = "1.4.0"
 LOG = logging.getLogger(__name__)
 
 
